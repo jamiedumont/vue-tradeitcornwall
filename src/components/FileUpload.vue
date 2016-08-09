15817f5d9985df5a2fadfb2ffa5ff54323b76333
Jamie Dumont <jamiedumont@icloud.com>
2016-08-09 09:07:39 +0100

    Tidy FileUpload component

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -5,16 +5,21 @@
             <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z" />
         </svg>
         <input
-            type="file"
-            name="files[]"
-            id="file"
-            accept="accept"
-            v-bind:multiple="multiple && advancedUpload"
-            v-on:change="onChange($event)" />
+          type="file"
+          name="files[]"
+          id="file"
+          accept="accept"
+          v-bind:multiple="multiple && advancedUpload"
+          v-on:change="onChange($event)"
+        />
         <label for="file">
-            <span v-if="fileList.length == 0"><strong>{{text.action}}</strong><span v-if="advancedUpload"> {{text.drag}}</span></span>
-            <!-- <span><strong>{{text.action}}</strong><span v-if="advancedUpload"> {{text.drag}}</span></span> -->
-            <span v-if="fileList.length > 0" class="">{{displaySelectionText}}</span>
+            <span v-if="fileList.length == 0">
+              <strong>{{text.action}}</strong>
+              <span v-if="advancedUpload"> {{text.drag}}</span>
+            </span>
+            <span v-if="fileList.length > 0">
+              {{displaySelectionText}}
+            </span>
         </label>
         <button type="submit" class="btn btn-primary" v-if="!hideButton && !autoSubmit" v-on:click.prevent="submitForm($event)">{{text.button}}</button>
     </div>
